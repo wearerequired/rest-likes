a0695020cbd5d0496df5f6bb7d710101e64840db
Silvan Hagen <neverything@gmail.com>
2016-10-13 17:48:10 +0200

    Add localStorage support for likes.
    - Check for localstorage
    - Set already liked posts early
    - Add & Remove posts from localstorage

--- js/rest-post-likes.js
+++ js/rest-post-likes.js
@@ -1,29 +1,83 @@
 ( function( $ ) {
-	var button = $( 'button.rest-post-like-button' );
+	var button = $( '.' + restPostLikes.button_classname );
+	var counter = $( '.' + restPostLikes.count_classname );
+	var storageKey = restPostLikes.storage_key;
+	var storage, fail, uid;
+	try {
+		uid = new Date;
+		(storage = window.localStorage).setItem(uid, uid);
+		fail = storage.getItem(uid) != uid;
+		storage.removeItem(uid);
+		fail && (storage = false);
+	} catch (exception) {}
 
-	button.on('click', function() {
-		console.log(button);
+	var getLikedPosts = function () {
+		if ( storage ) {
+			var storageData = storage.getItem( storageKey );
+			if ( storageData ) {
+				return JSON.parse( storageData );
+			}
+		}
+		return [];
+	};
 
-		var post_id = button.data('post-id');
-		var method = 'POST';
-		var nonce = button.data('nonce');
+	var addLikedPosts = function ( post_id ) {
+		if ( storage ) {
+			var storageData = getLikedPosts();
+			if ( storageData ) {
+				storageData.push( post_id );
+				storageData = _.uniq( storageData );
+				storage.setItem( storageKey, JSON.stringify( storageData ) );
+			}
+		}
+	};
 
-		if ( button.hasClass('liked') ) {
-			method = 'DELETE';
-			button.text( 'Unlike me' );
+	var removeLikedPost = function ( post_id ) {
+		if ( storage ) {
+			var storageData = getLikedPosts();
+			storageData = _.reject( storageData, function( num ) { return num === post_id } );
+			storageData = _.uniq( storageData );
+			storage.setItem( storageKey, JSON.stringify( storageData ) );
+		}
+	};
+
+	var buttonHandler = function () {
+
+		var post_id = button.data( 'post-id' );
+
+		if ( _.contains( getLikedPosts(), post_id ) ) {
+			button.toggleClass( restPostLikes.liked_classname );
 		}
 
-		$.ajax( {
-			url: wpApiSettings.root + 'rest-post-likes/v1/posts/' + post_id + '/like',
-			method: method,
-			beforeSend: function ( xhr ) {
-				xhr.setRequestHeader( 'X-WP-Nonce', wpApiSettings.nonce );
-			}
-		} ).done( function ( response ) {
-			console.log( response );
-			button.toggleClass( 'liked' );
-		} );
+		button.on( 'click', function () {
 
-	})
+			var method = 'POST';
+
+			if ( button.hasClass( restPostLikes.liked_classname ) ) {
+				method = 'DELETE';
+			}
+			$.ajax( {
+				url: wpApiSettings.root + 'rest-post-likes/v1/posts/' + post_id + '/like',
+				method: method,
+				beforeSend: function ( xhr ) {
+					xhr.setRequestHeader( 'X-WP-Nonce', wpApiSettings.nonce );
+				}
+			} ).done( function ( response ) {
+				if ( 'DELETE' === method ) {
+					removeLikedPost( post_id );
+				} else {
+					addLikedPosts( post_id );
+				}
+				button.toggleClass( restPostLikes.liked_classname );
+				counter.text( response.count );
+			} );
+		});
+	};
+
+	$( document ).ready( function () {
+		buttonHandler();
+	});
+
+
 
 })( jQuery );
