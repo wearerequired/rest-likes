2ab2c72c17c540cd6b096f84efb7bfb1189d52f5
Stefan Velthuys <velthy@required.ch>
2016-11-10 09:55:01 +0100

    use like-count as data-attr to allow styling for certain values

--- js/rest-post-likes.js
+++ js/rest-post-likes.js
@@ -104,7 +104,7 @@
 				// Remove processing class
 				$button.removeClass( restPostLikes.processing_classname );
 
-				$( '.' + restPostLikes.count_classname + '[data-post-id="' + post_id + '"]' ).text( response.count );
+				$( '.' + restPostLikes.count_classname + '[data-post-id="' + post_id + '"]' ).text( response.count ).attr( 'data-post-likes' , response.count );
 			} ).fail( function() {
 				// Toggle the button class to show interaction.
 				$button.toggleClass( restPostLikes.liked_classname );
