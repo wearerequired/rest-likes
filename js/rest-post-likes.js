a4127ce66ace5e3133da291f71ff38748df67998
Pascal Birchler <hello@pascalbirchler.ch>
2016-11-10 10:21:14 +0100

    Use delegated event handler
    Supports async post loading

--- js/rest-post-likes.js
+++ js/rest-post-likes.js
@@ -59,11 +59,9 @@
 		}
 	};
 
-	var buttonHandler = function () {
-		var $buttons = $( '.' + restPostLikes.button_classname );
-
+	var checkButtons = function() {
 		// Check localStorage for liked posts, set class on the buttons.
-		$buttons.each( function() {
+		$( '.' + restPostLikes.button_classname ).each( function() {
 			var $this = $( this );
 
 			if ( $this.hasClass( restPostLikes.liked_classname ) ) {
@@ -74,51 +72,59 @@
 				$this.toggleClass( restPostLikes.liked_classname );
 			}
 		} );
+	};
 
-		$buttons.on( 'click', function () {
-			var post_id = $( this ).data( 'post-id' ),
-			    $button = $( '.' + restPostLikes.button_classname + '[data-post-id="' + post_id + '"]' );
+	var buttonClickHandler = function( post_id ) {
+		var $button = $( '.' + restPostLikes.button_classname + '[data-post-id="' + post_id + '"]' );
 
-			// Set class while processing
-			$button.addClass( restPostLikes.processing_classname );
+		// Set class while processing
+		$button.addClass( restPostLikes.processing_classname );
 
-			// Define HTTP method.
-			var method = $button.hasClass( restPostLikes.liked_classname ) ? 'DELETE' : 'POST';
+		// Define HTTP method.
+		var method = $button.hasClass( restPostLikes.liked_classname ) ? 'DELETE' : 'POST';
 
+		// Toggle the button class to show interaction.
+		$button.toggleClass( restPostLikes.liked_classname );
+
+		$.ajax( {
+			url:        wpApiSettings.root + restPostLikes.endpoint_namespace + '/posts/' + post_id + '/like',
+			method:     method,
+			beforeSend: function( xhr ) {
+				xhr.setRequestHeader( 'X-WP-Nonce', wpApiSettings.nonce );
+			}
+		} ).done( function( response ) {
+			if ( 'DELETE' === method ) {
+				removeLikedPost( post_id );
+			} else {
+				addLikedPosts( post_id );
+			}
+
+			// Remove processing class
+			$button.removeClass( restPostLikes.processing_classname );
+
+			$( '.' + restPostLikes.count_classname + '[data-post-id="' + post_id + '"]' ).text( response.count ).attr( 'data-post-likes', response.count );
+		} ).fail( function() {
 			// Toggle the button class to show interaction.
 			$button.toggleClass( restPostLikes.liked_classname );
-
-			$.ajax( {
-				url: wpApiSettings.root + restPostLikes.endpoint_namespace + '/posts/' + post_id + '/like',
-				method: method,
-				beforeSend: function ( xhr ) {
-					xhr.setRequestHeader( 'X-WP-Nonce', wpApiSettings.nonce );
-				}
-			} ).done( function ( response ) {
-				if ( 'DELETE' === method ) {
-					removeLikedPost( post_id );
-				} else {
-					addLikedPosts( post_id );
-				}
-
-				// Remove processing class
-				$button.removeClass( restPostLikes.processing_classname );
-
-				$( '.' + restPostLikes.count_classname + '[data-post-id="' + post_id + '"]' ).text( response.count ).attr( 'data-post-likes' , response.count );
-			} ).fail( function() {
-				// Toggle the button class to show interaction.
-				$button.toggleClass( restPostLikes.liked_classname );
-				// Remove processing class
-				$button.removeClass( restPostLikes.processing_classname );
-			} );
-		});
+			// Remove processing class
+			$button.removeClass( restPostLikes.processing_classname );
+		} );
 	};
 
 	/**
 	 * Hook up the handler.
 	 */
-	$( document ).ready( function () {
-		buttonHandler();
-	});
+	$( document ).ready( function() {
+		checkButtons();
 
+		$( document.body ).on( 'post-load', function() {
+			checkButtons();
+		} );
+
+		$( 'document' ).on( 'click', '.' + restPostLikes.button_classname, function() {
+			var post_id = $( this ).data( 'post-id' );
+
+			buttonClickHandler( post_id );
+		} );
+	} );
 })( jQuery );
