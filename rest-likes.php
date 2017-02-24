e11c6c33a9890031d05f61612c9962e88ea766c7
Pascal Birchler <hello@pascalbirchler.ch>
2017-02-24 11:22:59 +0100

    Include WP_Requirements_Check

--- rest-likes.php
+++ rest-likes.php
@@ -15,56 +15,61 @@
  * @package rest-likes
  */
 
-if ( file_exists( dirname( __FILE__ ) . '/vendor/autoload.php' ) ) {
-	include( dirname( __FILE__ ) . '/vendor/autoload.php' );
-}
+$requirements_check = new WP_Requirements_Check( array(
+	'title' => __( 'REST Likes', 'digest' ),
+	'php'   => '5.6',
+	'wp'    => '4.7',
+	'file'  => __FILE__,
+) );
 
-/**
- * Load the plugin on plugins_loaded.
- *
- * @return Required\RestLikes\Plugin
- */
-function rest_likes() {
-	static $controller = null;
+if ( $requirements_check->passes() ) {
+	/**
+	 * Load the plugin on plugins_loaded.
+	 *
+	 * @return Required\RestLikes\Plugin
+	 */
+	function rest_likes() {
+		static $controller = null;
 
-	if ( null === $controller ) {
-		$controller = new \Required\RestLikes\Plugin();
+		if ( null === $controller ) {
+			$controller = new \Required\RestLikes\Plugin();
+		}
+
+		return $controller;
 	}
 
-	return $controller;
-}
+	add_action( 'plugins_loaded', [ rest_likes(), 'add_hooks' ] );
 
-add_action( 'plugins_loaded', [ rest_likes(), 'add_hooks' ] );
+	/**
+	 * Get post like count.
+	 *
+	 * @param int $post_id WP_Post ID.
+	 *
+	 * @return int
+	 */
+	function get_rest_post_like_count( $post_id ) {
+		return rest_likes()->get_like_count( 'post', $post_id );
+	}
 
-/**
- * Get post like count.
- *
- * @param int $post_id WP_Post ID.
- *
- * @return int
- */
-function get_rest_post_like_count( $post_id ) {
-	return rest_likes()->get_like_count( 'post', $post_id );
-}
+	/**
+	 * Get post like count markup.
+	 *
+	 * @param int $post_id WP_Post ID.
+	 *
+	 * @return string
+	 */
+	function the_rest_post_like_count( $post_id ) {
+		return rest_likes()->get_like_count_html( 'post', $post_id );
+	}
 
-/**
- * Get post like count markup.
- *
- * @param int $post_id WP_Post ID.
- *
- * @return string
- */
-function the_rest_post_like_count( $post_id ) {
-	return rest_likes()->get_like_count_html( 'post', $post_id );
-}
-
-/**
- * Get the button markup.
- *
- * @param int $post_id WP_Post ID.
- *
- * @return string|WP_Error
- */
-function get_rest_post_like_button( $post_id ) {
-	return rest_likes()->get_like_button( 'post', $post_id );
+	/**
+	 * Get the button markup.
+	 *
+	 * @param int $post_id WP_Post ID.
+	 *
+	 * @return string|WP_Error
+	 */
+	function get_rest_post_like_button( $post_id ) {
+		return rest_likes()->get_like_button( 'post', $post_id );
+	}
 }
