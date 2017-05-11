f34df7d5adde02d20e3a014098123369aa313812
Pascal Birchler <hello@pascalbirchler.ch>
2017-05-11 12:40:47 +0200

    Add `Domain Path` header
    See #46.

--- rest-likes.php
+++ rest-likes.php
@@ -9,6 +9,7 @@
  * Author:      Silvan Hagen
  * Author URI:  https://required.com
  * Text Domain: rest-likes
+ * Domain Path: /languages
  * License:     GPL-2.0+
  * License URI: http://www.gnu.org/licenses/gpl-2.0.txt
  *
