c69ff43a21b20f065a55a9e818a94cb770d4017d
Pascal Birchler <hello@pascalbirchler.ch>
2017-09-14 11:41:53 +0200

    Use ES6 with Babel

--- js/rest-likes.js
+++ js/rest-likes.js
@@ -1,190 +1,196 @@
-(function( document, window, $, restLikes, wp ) {
+'use strict';
+
+(function (document, window, $, restLikes, wp) {
 	/**
-	 * Check for localStorage support in the browser.
-	 */
-	var storage, fail, uid;
+  * Check for localStorage support in the browser.
+  */
+	var storage = void 0;
+	var fail = void 0;
+	var uid = void 0;
+
 	try {
-		uid = new Date;
-		(storage = window.localStorage).setItem( uid, uid );
-		fail = storage.getItem( uid ) != uid;
-		storage.removeItem( uid );
+		uid = new Date();
+		(storage = window.localStorage).setItem(uid, uid);
+		fail = storage.getItem(uid) != uid;
+		storage.removeItem(uid);
 		fail && (storage = false);
-	} catch ( exception ) {
-	}
+	} catch (exception) {}
 
 	/**
-	 * Check for CustomEvent support in the browser.
-	 *
-	 * @url https://developer.mozilla.org/en-US/docs/Web/API/CustomEvent/CustomEvent#Polyfill
-	 */
-	if ( typeof window.CustomEvent !== 'function' ) {
-		function CustomEvent( event, params ) {
-			params  = params || { bubbles: false, cancelable: false, detail: undefined };
-			var evt = document.createEvent( 'CustomEvent' );
-			evt.initCustomEvent( event, params.bubbles, params.cancelable, params.detail );
+  * Check for CustomEvent support in the browser.
+  *
+  * @url https://developer.mozilla.org/en-US/docs/Web/API/CustomEvent/CustomEvent#Polyfill
+  */
+	if (typeof window.CustomEvent !== 'function') {
+		var _CustomEvent = function _CustomEvent(event) {
+			var params = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : { bubbles: false, cancelable: false, detail: undefined };
+
+			var evt = document.createEvent('CustomEvent');
+			evt.initCustomEvent(event, params.bubbles, params.cancelable, params.detail);
 			return evt;
-		}
+		};
 
-		CustomEvent.prototype = window.Event.prototype;
-		window.CustomEvent = CustomEvent;
+		_CustomEvent.prototype = window.Event.prototype;
+		window.CustomEvent = _CustomEvent;
 	}
 
 	/**
-	 * Get liked posts from localStorage.
-	 *
-	 * @returns {Array}
-	 */
-	var getLikedItems = function( objectType ) {
-		if ( storage ) {
-			var storageData = storage.getItem( objectType );
-			if ( storageData ) {
-				return JSON.parse( storageData );
+  * Get liked posts from localStorage.
+  *
+  * @returns {Array}
+  */
+	var getLikedItems = function getLikedItems(objectType) {
+		if (storage) {
+			var storageData = storage.getItem(objectType);
+			if (storageData) {
+				return JSON.parse(storageData);
 			}
 		}
 		return [];
 	};
 
 	/**
-	 * Add like to localStorage.
-	 *
-	 * @param {string} objectType
-	 * @param {number} objectId
-	 */
-	var addLikedItem = function( objectType, objectId ) {
-		if ( storage ) {
+  * Add like to localStorage.
+  *
+  * @param {string} objectType
+  * @param {number} objectId
+  */
+	var addLikedItem = function addLikedItem(objectType, objectId) {
+		if (storage) {
 			var storageData = getLikedItems();
-			if ( storageData ) {
-				storageData.push( objectId );
-				storageData = _.unique( storageData );
-				storage.setItem( objectType, JSON.stringify( storageData ) );
+			if (storageData) {
+				storageData.push(objectId);
+				storageData = _.unique(storageData);
+				storage.setItem(objectType, JSON.stringify(storageData));
 			}
 		}
 	};
 
 	/**
-	 * Remove like from localStorage.
-	 *
-	 * @param {string} objectType
-	 * @param {number} objectId
-	 */
-	var removeLikedItem = function( objectType, objectId ) {
-		if ( storage ) {
-			var storageData = getLikedItems( objectType );
-			storageData     = _.reject( storageData, function( num ) {
-				return num === objectId
-			} );
-			storageData     = _.unique( storageData );
-			storage.setItem( objectType, JSON.stringify( storageData ) );
+  * Remove like from localStorage.
+  *
+  * @param {string} objectType
+  * @param {number} objectId
+  */
+	var removeLikedItem = function removeLikedItem(objectType, objectId) {
+		if (storage) {
+			var storageData = getLikedItems(objectType);
+			storageData = _.reject(storageData, function (num) {
+				return num === objectId;
+			});
+			storageData = _.unique(storageData);
+			storage.setItem(objectType, JSON.stringify(storageData));
 		}
 	};
 
-	var checkButtons = function() {
+	var checkButtons = function checkButtons() {
 		// Check localStorage for liked items, set class on the buttons.
-		$( '[data-rest-like-button]' ).each( function() {
-			var $this      = $( this ),
-			    type       = $this.data( 'type' ),
-			    classNames = restLikes.object_types[ type ].classnames;
+		$('[data-rest-like-button]').each(function () {
+			var $this = $(this);
+			var type = $this.data('type');
+			var classNames = restLikes.object_types[type].classnames;
 
-			if ( $this.hasClass( classNames.liked ) ) {
+			if ($this.hasClass(classNames.liked)) {
 				return;
 			}
 
-			if ( _.contains( getLikedItems( type ), $this.data( 'id' ) ) ) {
-				$this.toggleClass( classNames.liked );
-				$this.find( '.' + classNames.label ).html( restLikes.object_types[ type ].texts.unlike );
+			if (_.contains(getLikedItems(type), $this.data('id'))) {
+				$this.toggleClass(classNames.liked);
+				$this.find('.' + classNames.label).html(restLikes.object_types[type].texts.unlike);
 			}
-		} );
+		});
 	};
 
 	/**
-	 *
-	 * @param {string} objectType
-	 * @param {number} objectId
-	 */
-	var buttonClickHandler = function( objectType, objectId ) {
+  *
+  * @param {string} objectType
+  * @param {number} objectId
+  */
+	var buttonClickHandler = function buttonClickHandler(objectType, objectId) {
 		// Get all buttons for that specific object.
-		var $button    = $( '[data-rest-like-button][data-type="' + objectType + '"][data-id="' + objectId + '"]' ),
-		    objectType = restLikes.object_types[ objectType ],
-		    classNames = objectType.classnames;
+		var $button = $('[data-rest-like-button][data-type="' + objectType + '"][data-id="' + objectId + '"]');
+
+		var objectTypeData = restLikes.object_types[objectType];
+		var classNames = objectTypeData.classnames;
 
 		// Set class while processing.
-		$button.addClass( classNames.processing );
+		$button.addClass(classNames.processing);
 
 		// Define HTTP method.
-		var method = $button.hasClass( classNames.liked ) ? 'DELETE' : 'POST';
+		var method = $button.hasClass(classNames.liked) ? 'DELETE' : 'POST';
 
 		// Toggle the button class to show interaction.
-		$button.toggleClass( classNames.liked );
+		$button.toggleClass(classNames.liked);
 
-		$.ajax( {
-			url:    restLikes.root + objectType.endpoint.replace( '%s', objectId ),
+		$.ajax({
+			url: restLikes.root + objectTypeData.endpoint.replace('%s', objectId),
 			method: method,
-			beforeSend: function( xhr ) {
-				if ( restLikes.nonce ) {
-					xhr.setRequestHeader( 'X-WP-Nonce', restLikes.nonce );
+			beforeSend: function beforeSend(xhr) {
+				if (restLikes.nonce) {
+					xhr.setRequestHeader('X-WP-Nonce', restLikes.nonce);
 				}
 			}
-		} ).done( function( response ) {
+		}).done(function (response) {
 			// Remove processing class
-			$button.removeClass( classNames.processing );
+			$button.removeClass(classNames.processing);
 
-			$button.find( '.' + classNames.count ).text( response.countFormatted ).attr( 'data-likes', response.count );
+			$button.find('.' + classNames.count).text(response.countFormatted).attr('data-likes', response.count);
 
-			if ( 'DELETE' === method ) {
-				removeLikedItem( objectType, objectId );
+			if ('DELETE' === method) {
+				removeLikedItem(objectType, objectId);
 
-				$button.find( '.' + classNames.label ).html( objectType.texts.like );
+				$button.find('.' + classNames.label).html(objectTypeData.texts.like);
 
-				wp.a11y.speak( restLikes.l10n.unlikeMsg.replace( '%s', response.count ), 'polite' );
+				wp.a11y.speak(restLikes.l10n.unlikeMsg.replace('%s', response.count), 'polite');
 
-				document.dispatchEvent( new CustomEvent( 'restLikes', {
+				document.dispatchEvent(new CustomEvent('restLikes', {
 					detail: {
-						'action':         'unlike',
-						'count':          response.count,
-						'countFormatted': response.countFormatted,
+						'action': 'unlike',
+						'count': response.count,
+						'countFormatted': response.countFormatted
 					}
-				} ) );
+				}));
 			} else {
-				addLikedItem( objectType, objectId );
+				addLikedItem(objectType, objectId);
 
-				$button.find( '.' + classNames.label ).html( objectType.texts.unlike );
+				$button.find('.' + classNames.label).html(objectTypeData.texts.unlike);
 
-				wp.a11y.speak( restLikes.l10n.likeMsg.replace( '%s', response.count ), 'polite' );
+				wp.a11y.speak(restLikes.l10n.likeMsg.replace('%s', response.count), 'polite');
 
-				document.dispatchEvent( new CustomEvent( 'restLikes', {
+				document.dispatchEvent(new CustomEvent('restLikes', {
 					detail: {
-						'action':         'like',
-						'count':          response.count,
-						'countFormatted': response.countFormatted,
+						'action': 'like',
+						'count': response.count,
+						'countFormatted': response.countFormatted
 					}
-				} ) );
+				}));
 			}
-		} ).fail( function() {
-			$button.toggleClass( classNames.liked ).removeClass( classNames.processing );
+		}).fail(function () {
+			$button.toggleClass(classNames.liked).removeClass(classNames.processing);
 
-			wp.a11y.speak( restLikes.l10n.errorMsg, 'polite' );
+			wp.a11y.speak(restLikes.l10n.errorMsg, 'polite');
 
-			document.dispatchEvent( new CustomEvent( 'restLikes', {
+			document.dispatchEvent(new CustomEvent('restLikes', {
 				detail: {
-					'action': 'error',
+					'action': 'error'
 				}
-			} ) );
-		} );
+			}));
+		});
 	};
 
 	/**
-	 * Hook up the handler.
-	 */
-	$( document ).ready( function() {
+  * Hook up the handler.
+  */
+	$(document).ready(function () {
 		checkButtons();
 
 		// Allow triggering a custom event to check buttons again.
-		$( document.body ).on( 'restLikes', function() {
+		$(document.body).on('restLikes', function () {
 			checkButtons();
-		} );
+		});
 
-		$( document.body ).on( 'click', '[data-rest-like-button]', function() {
-			buttonClickHandler( $( this ).data( 'type' ), $( this ).data( 'id' ) );
-		} );
-	} );
-})( document, window, jQuery, window.restLikes, wp );
+		$(document.body).on('click', '[data-rest-like-button]', function () {
+			buttonClickHandler($(this).data('type'), $(this).data('id'));
+		});
+	});
+})(document, window, jQuery, restLikes, wp);
