7f7766f8493818fe72b2549c5c96d03b327ec6e1
Dominik Schilling <dominikschilling+git@gmail.com>
2022-11-06 13:54:41 +0100

    Update dependencies and remove legacyBrowsers.js entry

--- js/src/legacyBrowsers.js
+++
@@ -1,9 +0,0 @@
-/**
- * Polyfills
- */
-import 'whatwg-fetch';
-import 'core-js/stable';
-import 'regenerator-runtime/runtime';
-import 'element-closest';
-
-import './rest-likes';
--- js/src/modernBrowsers.js
+++
@@ -1,1 +0,0 @@
-import './rest-likes';
