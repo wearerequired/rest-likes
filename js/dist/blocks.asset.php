6551fbb85b2f7bd966849b37ae99e47c6e03af32
Stefan Pasch <stefan.pasch@gmail.com>
2024-08-02 15:35:27 +0200

    Build assets

---
+++ js/dist/blocks.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-primitives'), 'version' => 'bcfc92bc46d90cef6d8a');
---
+++ js/dist/rest-likes.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('jquery', 'wp-a11y', 'wp-i18n'), 'version' => '2ffc331695a9f8ed45a9');
