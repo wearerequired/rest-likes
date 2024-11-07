8ea276bf0a7b91cb2eb0a26f6c35a3782916225a
Jeff Chi 杰夫迟 <info@spinken.net>
2024-11-07 12:31:33 +0100

    Feature/like count in block (#150)
    * Fetch actual like count in block
    
    * Fetch actual like count for editor comments block
    
    * Improve loading & error states
    
    * Simplify by using `useEntityProp`
    
    * Add spinner when not loaded yet

--- js/dist/blocks.asset.php
+++ js/dist/blocks.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => '24cfa03e3bdc36411dce');
+<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-i18n'), 'version' => 'f027d158efe40d061cbb');
--- js/src/blocks/comment-like-button/edit.tsx
+++ js/src/blocks/comment-like-button/edit.tsx
@@ -2,22 +2,35 @@
  * WordPress dependencies
  */
 import { __, _n, sprintf } from '@wordpress/i18n';
+import { Spinner } from '@wordpress/components';
+import { useEntityProp } from '@wordpress/core-data';
 import { useBlockProps } from '@wordpress/block-editor';
 
-const Edit = (): JSX.Element => {
+const Edit = ( { context }: { context: { commentId: number } } ): JSX.Element => {
 	const blockProps = useBlockProps();
+	const [ likeCount ] = useEntityProp< number >(
+		'root',
+		'comment',
+		'_rest_likes',
+		context.commentId
+	);
+
+	if ( ! likeCount && 0 !== likeCount ) {
+		return <Spinner />;
+	}
 
 	return (
 		<div { ...blockProps }>
 			<button type="button" className="rest-like-button">
 				<span className="rest-like-button-label">{ __( 'Like', 'rest-likes' ) }</span>
 				<span className="rest-like-count">
-					<span aria-hidden="true">37</span>
+					<span aria-hidden="true">{ likeCount }</span>
 					<span className="screen-reader-text">
-						{
+						{ sprintf(
 							/* translators: %s: number of likes */
-							sprintf( _n( '%s likes', '%s likes', 37, 'rest-likes' ), 37 )
-						}
+							_n( '%s likes', '%s likes', likeCount, 'rest-likes' ),
+							likeCount
+						) }
 					</span>
 				</span>
 			</button>
