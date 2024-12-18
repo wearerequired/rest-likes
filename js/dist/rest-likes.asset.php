525f8c5cac832d30789f8710e45d8176c1e1a015
Dominik Schilling <dominikschilling+git@gmail.com>
2024-12-18 17:46:53 +0100

    Observe attributes to update buttons on class changes (#151)

--- js/dist/rest-likes.asset.php
+++ js/dist/rest-likes.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('jquery', 'wp-a11y', 'wp-i18n'), 'version' => '9932c0caed63020dba0c');
+<?php return array('dependencies' => array('jquery', 'wp-a11y', 'wp-i18n'), 'version' => '596307d02840c159683e');
